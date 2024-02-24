63eac715cadeef416310d02e2a2b38205d9d3754
Jhon Cruz <jhonskaz@gmail.com>
2024-02-24 10:52:17 -0300

    Adiciona NavMenu e NavItens

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { MainNav } from "./MainNavegation";
 import { Logo } from "./logo";
 import { Search } from "lucide-react";
 export function Sidebar() {
@@ -5,10 +6,11 @@ export function Sidebar() {
     <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
         <Logo />
 
-        <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 text-zinc-500"></Search>
         <input className="flex-1 outline-none border-none bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search..." type="text" />
         </div>
+        <MainNav />
     </aside>
     )
 }
