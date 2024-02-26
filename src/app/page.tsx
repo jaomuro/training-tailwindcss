7ca214245d962d373fc4da5b2c1d9d3ffb3b5385
Jhon Cruz <jhonskaz@gmail.com>
2024-02-26 09:09:57 -0300

    Adiciona estrutura inicial do form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,34 @@ import { SettingsTabs } from '@/components/SetteingsTabs.tsx';
 
 export default function Home() {
   return (
-    <div>
       <>
       <h1 className="font-medium text-zinc-900 text-2xl">Settings</h1>
       <SettingsTabs />
+
+
+
+      <div className='mt-6  flex flex-col'>
+
+        <div className='flex items-center justify-between border-b border-zinc-200 pb-5'>
+
+          <div className='space-y-1'>
+            <h2 className='text-lg font-medium text-zinc-900'>Personal info</h2>
+            <p className='text-sm text-zinc-500'>Update your photo and personal details here.</p>
+          </div>
+
+          <div className='flex items-center gap-2'>
+            <button type='button' className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50'>Cancel</button>
+            <button type='submit' form='settings' className='rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700'>Save</button>
+          </div>
+
+        </div>
+
+        <form id='settings' className='mt-6 flex flex-col w-full'>
+          
+        </form>
+      </div>
+
+
       </>
-    </div>
   )
 }
