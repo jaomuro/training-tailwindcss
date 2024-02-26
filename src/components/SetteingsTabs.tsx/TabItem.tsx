f76fa8e5ea70b509a30424c26b821df99da1f786
Jhon Cruz <jhonskaz@gmail.com>
2024-02-26 08:44:47 -0300

    Adiciona animação nas tabs com motion

--- src/components/SetteingsTabs.tsx/TabItem.tsx
+++ src/components/SetteingsTabs.tsx/TabItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs';
+import { motion } from 'framer-motion'
 
 interface TabItemProps {
     value: string
@@ -15,7 +16,7 @@ export function TabItem({value, title, isSelected=false}: TabItemProps) {
         <span>{title}</span>
 
         {isSelected && (
-            <div className='absolute left-0 right-0 h-0.5 bg-violet-700 -bottom-px'/>
+            <motion.div layoutId='activeTab' className='absolute left-0 right-0 h-0.5 bg-violet-700 -bottom-px'/>
         )}
         </Tabs.Trigger>
     )
